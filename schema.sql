91063290738dafd594274bf982496e091c1d8936
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-08-24 20:04:05 +0300

    create queries, data, and schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
-);
+  id int NOT NULL,
+  name varchar NOT NULL,
+  date_of_birth date NOT NULL,
+  escape_attempts int NOT NULL,
+  neutere BOOLEAN NOT NULL,
+  weight_kg decimal NOT NULL
+) 
