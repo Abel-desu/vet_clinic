f3e488b7dd0d51394d79f2ef94b5e6493b77ccaa
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-08-27 18:03:33 +0300

    change required

--- data.sql
+++ data.sql
@@ -45,3 +45,12 @@ Update animals Set owner_id = 3WHERE name IN('Devimon', 'Plantmon');
 
 Update animals Set owner_id = 5 WHERE name IN('Angemon','Boarmon');
 
+BEGIN;
+INSERT INTO vets(name,age,date_of_graduation)
+VALUES('William Tatcher', 45, '2000-04-23'), 
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'),
+  ('Jack Harkness', 38, '2008-06-08')
+;
+COMMIT;
+
--- schema.sql
+++ schema.sql
@@ -7,3 +7,36 @@ CREATE TABLE animals (
   weight_kg decimal(5,0) NOT NULL,
 ) 
 alter table animals add column species VARCHAR(20);
+
+CREATE TABLE owners(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(50),
+    age INT,
+    PRIMARY KEY(id)
+    
+);
+
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50), 
+    PRIMARY KEY(id)
+);
+
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT species_id
+    FOREIGN KEY (species_id)
+     REFERENCES species(id)
+
+;
+
+ALTER TABLE animals ADD owner_id INT;
+
+ALTER TABLE animals
+    ADD CONSTRAINT owner_id
+        FOREIGN KEY (owner_id)
+            REFERENCES owners(id)
+
